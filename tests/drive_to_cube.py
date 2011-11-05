fe8762fc20ac465541372ea4b6f6d83b696784bd
Eric Wieser <wieser.eric@gmail.com>
2011-11-05 22:26:05 +0000

    Moved to tests folder

---
+++ tests/drive_to_cube.py
@@ -0,0 +1,26 @@
+from sr import *
+import time
+import math
+from systemetricRobot import SystemetricRobot
+
+R = SystemetricRobot()
+
+speed = 50
+
+while True:
+    markers = R.see()
+    if len(markers) != 0:
+        print "Saw the marker"
+        angle = markers[0].centre.polar.rot_y
+        if math.fabs(angle) < 10:
+            R.left = R.right = speed
+        else:
+            R.left = angle
+            R.right = -angle
+            time.sleep(0.25)
+            R.stop()
+        
+        print(angle)
+    else:
+        print "No marker"
+        R.stop()
