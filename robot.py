951c8d01fc2f7f140addafe6f1967281d0a528da
Eric Wieser <wieser.eric@gmail.com>
2011-11-05 22:28:29 +0000

    Moved into a new file

--- robot.py
+++ robot.py
@@ -1,45 +1,5 @@
-from sr import *
-import time
-import math
-from systemetricRobot import SystemetricRobot
+#load the test program from tests/drive_to_cube.py
+from tests import drive_to_cube
 
-R = SystemetricRobot()
-
-speed = 50
-
-while True:
-    markers = R.see()
-    if len(markers) != 0:
-        print "Saw the marker"
-        angle = markers[0].centre.polar.rot_y
-        if math.fabs(angle) < 10:
-            R.left = R.right = speed
-        else:
-            R.left = angle
-            R.right = -angle
-            time.sleep(0.25)
-            R.stop()
-        
-        print(angle)
-    else:
-        print "No marker"
-        R.stop()
-    
-'''
-while True:
-    markers = R.see()
-    if len(markers) != 0:
-        print "Saw the marker"
-        targetPoint = markers[0].centre
-        steer = targetPoint.polar.rot_y * 5
-        
-        print(steer)
-        
-        R.right = speed - steer 
-        R.left = speed + steer 
-    else:
-        print "No marker"
-        R.right = 0
-        R.left = 0
-        
-    time.sleep(0.1)'''
+#run the program
+drive_to_cube.main()
