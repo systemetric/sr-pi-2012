ab7f3291fb5e4efabb881d8cb9c2f81e8159fd2b
Nic Dart <nicholasjdart@gmail.com>
2011-11-08 17:33:55 +0000

    Minor changes

--- tests/Drive_to_Cube_Better.py
+++ tests/Drive_to_Cube_Better.py
@@ -11,17 +11,16 @@ def main():
     while True:
         markers = R.see()
         if len(markers) != 0:
-            print "Saw the marker"
             angle = markers[0].centre.polar.rot_y
+            print angel
             if math.fabs(angle) < 10:
                 R.left = R.right = speed
             else:
                 R.left = angle
                 R.right = -angle
-                time.sleep(0.25)
+                time.sleep(0.1)
                 R.stop()
             
             print(angle)
         else:
-            print "No marker"
             R.stop()
