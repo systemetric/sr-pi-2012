a7da5449de7191af5959c6f711a2c7b2232d6e15
Nic Dart <nicholasjdart@gmail.com>
2011-11-09 15:50:00 +0000

    Fix Johns code

--- tests/drive_to_cube_better.py
+++ tests/drive_to_cube_better.py
@@ -16,14 +16,14 @@ def main():
         if len(markers) != 0:        #if there is A valid QR there...
             print "Saw the marker"
             angle = markers[0].centre.polar.rot_y    #set the angle of the object (from center)
-            while math.fabs(angle) < 10:                #set angle to an absolute value, then drive @ angle
+            if math.fabs(angle) < 10:                #set angle to an absolute value, then drive @ angle
                 R.drive(speed)
             else:                                    #else, stop
                 R.turn(angle)
                 #time.sleep(0.25)
                 R.stop()
-            
             print "seen at: ",angle
+            R.stop();
         else:
             R.turn(20)
             time.sleep(0.15)
