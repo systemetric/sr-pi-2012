60883ff07bc56005989f51fd8e9c2b8ce592c7fc
Nic Dart <nicholasjdart@gmail.com>
2011-11-09 16:29:32 +0000

    Cam_refresh attempt #1

--- robot.py
+++ robot.py
@@ -1,5 +1,7 @@
 #load the test program from tests/drive_to_cube_better.py
-from tests import drive_to_cube_better
+#from tests import drive_to_cube_better
+from tests import cam_refresh.py
 
 #run the program
-drive_to_cube_better.main()
+#drive_to_cube_better.main()
+cam_refresh.main()
