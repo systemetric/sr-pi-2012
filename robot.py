11e9233bbec00b7af3b97dd2f858cc36df093d30
Nic Dart <nicholasjdart@gmail.com>
2011-11-09 16:29:55 +0000

    Commit messages

--- robot.py
+++ robot.py
@@ -1,6 +1,6 @@
 #load the test program from tests/drive_to_cube_better.py
 #from tests import drive_to_cube_better
-from tests import cam_refresh.py
+from tests import cam_refresh
 
 #run the program
 #drive_to_cube_better.main()
