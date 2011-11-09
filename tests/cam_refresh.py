893df0c9df7e42dc05a9898866ddddbc5eb025d1
Jonathan Laver <jonathan.laver@gmail.com>
2011-11-09 16:58:02 +0000

    made it stop until it sees the marker

--- tests/cam_refresh.py
+++ tests/cam_refresh.py
@@ -4,16 +4,21 @@ import math
 from systemetricRobot import SystemetricRobot
 
 def main():
+    res = (320, 240)
     R = SystemetricRobot()
     
     
    # markers = [marker for marker in allMarkers if marker.info.marker_type == MARKER_TOKEN] #getting the valid QR code
-    
+    while True:
+        markers = R.see(res=res)
+        if len(markers) != 0:
+            break
+        R.power.beep((440,1))
     for i in range(21,0,-1):
         R.turn(i)
         time.sleep(1)
         markers = R.see( res=(640, 360) )
-        if len(markers) != 0:        #if there is A valid liboki there...
+        if len(markers) != 0:        #if there is A valid QR there...
             print "Found, speed of ",i
             R.power.beep([(440,1), (220,1)])
             break
