827b7e1a702db811a14e9419e56af05c8848d576
Louis Taylor <kragniz@gmail.com>
2011-11-09 22:10:57 +0000

    Made code look nice.

--- systemetricRobot.py
+++ systemetricRobot.py
@@ -1,7 +1,7 @@
 from sr import *
 
 class SystemetricRobot(Robot):
-    """A class derived from the base `Robot` class provided by soton"""
+    '''A class derived from the base 'Robot' class provided by soton'''
     def __init__(self):
         #Make sure the soton class is initiated, so we can connect to motors
         Robot.__init__(self)
@@ -13,31 +13,33 @@ class SystemetricRobot(Robot):
     #Getters and setters for left motor. Allows you to write R.left = 100 for full speed forward
     @property
     def left(self):
-        """Property to control the speed of the left motor, without the hassle of `.target`"""
+        '''Property to control the speed of the left motor, without the hassle of '.target''''
         return self.leftMotor.target
+        
     @left.setter
     def left(self, value):
         self.leftMotor.target = value
         
     @property  
     def right(self):
-        """Property to control the speed of the right motor, without the hassle of `.target`"""
+        '''Property to control the speed of the right motor, without the hassle of '.target''''
         return self.rightMotor.target
+        
     @right.setter     
     def right(self, value):
         self.rightMotor.target = value
     
     def stop(self):
-        """Stop the robot, by setting the speed of both motors to 0"""
-        self.right = self.left = 0
+        '''Stop the robot, by setting the speed of both motors to 0'''
+        self.right = 0
+        self.left = 0
         
     def drive(self, speed=50, steer=0):
-        """Drive the robot forwards or backwards at a certain speed, with an optional steer
-        """
+        '''Drive the robot forwards or backwards at a certain speed, with an optional steer'''
         self.right = speed - steer
         self.left = speed + steer
         
     def turn(self, speed):
-        """Rotate the robot at a certain speed. Positive is clockwise"""
+        '''Rotate the robot at a certain speed. Positive is clockwise'''
         self.right = -speed
         self.left = speed
