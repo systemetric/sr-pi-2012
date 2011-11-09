a27d91e06a3f70cc922e13e3097f68d329e334a6
Louis Taylor <kragniz@gmail.com>
2011-11-09 22:04:33 +0000

    Made testing a bit cleaner

--- robot.py
+++ robot.py
@@ -1,7 +1,2 @@
-#load the test program from tests/drive_to_cube_better.py
-#from tests import drive_to_cube_better
-from tests import cam_refresh
-
-#run the program
-#drive_to_cube_better.main()
-cam_refresh.main()
+#run some test
+tests.main()
