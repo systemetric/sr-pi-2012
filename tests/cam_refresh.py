2f2d9cec814e2a224053873e8ba6807bf28c0e56
Nic Dart <nicholasjdart@gmail.com>
2011-11-09 16:34:25 +0000

    some changes

--- tests/cam_refresh.py
+++ tests/cam_refresh.py
@@ -6,12 +6,13 @@ from systemetricRobot import SystemetricRobot
 def main():
     R = SystemetricRobot()
     
-    allMarkers = R.see()
-    markers = [marker for marker in allMarkers if marker.info.marker_type == MARKER_TOKEN] #getting the valid QR code
+    
+   # markers = [marker for marker in allMarkers if marker.info.marker_type == MARKER_TOKEN] #getting the valid QR code
     
     for i in range(21,0,-1):
         R.turn(i)
         time.sleep(1)
+        markers = R.see()
         if len(markers) != 0:        #if there is A valid QR there...
             print "Found, speed of ",i
             R.power.beep([(440,1), (220,1)])
