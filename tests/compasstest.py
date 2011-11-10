12bd691dd964564f421f14339d6725578d6ad870
Louis Taylor <kragniz@gmail.com>
2011-11-10 17:18:37 +0000

    Basic feedback

--- tests/compasstest.py
+++ tests/compasstest.py
@@ -1,11 +1,19 @@
 import time
 from systemetricRobot import SystemetricRobot
 
-def main():
+def main:
     R = SystemetricRobot()
     target = 0
     
     while True:
-        heading = R.compassHeading
+        heading = robot.heading
+        
+        error = heading - target
+        while error >= 180:
+            error -= 360
+        while error < -180:
+            error += 360
+            
         print heading
+        R.turn(error/3)
         time.sleep(0.2)
