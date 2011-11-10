23422b9d985c274f646963ebd98b812d6d5096d1
Nic Dart <nicholasjdart@gmail.com>
2011-11-10 16:32:30 +0000

    More basic

--- tests/serialTest.py
+++ tests/serialTest.py
@@ -7,18 +7,11 @@ def main():
     R = SystemetricRobot()
     
     port = serial.Serial('/dev/ttyACM0')
+    
     port.open()
     port.timeout=0.25
-    angle = 90000 #random value
-    t = 0
-    while angle > 10:
-        t += 1
-        if t > 100:
-            R.turn(8)
-        angle = float(port.readline())
-        print angle
-        time.sleep(0.2)
-        
-    print 'We finished!'
-    R.turn(0)
     
+    while True:
+        angle = int(port.readline()) / 10
+        print angle
+        time.sleep(0.2)    
