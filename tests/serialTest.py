c80b6ed17b2e3d4690c2e9da6a0effbf5b05b06a
Louis Taylor <kragniz@gmail.com>
2011-11-10 12:59:53 +0000

    Turn until facing <3 degrees

--- tests/serialTest.py
+++ tests/serialTest.py
@@ -1,13 +1,19 @@
 import serial
 import glob
 import time
+from systemetricRobot import SystemetricRobot
 
 def main():
+    R = SystemetricRobot()
+    
     port = serial.Serial('/dev/ttyACM0')
     port.open()
     port.timeout=0.25
+    angle = 200 #random value
     
-    while True:
-        line = port.readline()
-        print line
+    while angle > 3:
+        angle = float(port.readline())
+        print angle
+        R.turn(15)
+        time.sleep(1)
     
