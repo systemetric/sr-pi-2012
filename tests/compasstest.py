56381b765e1401011f05bd0b0ae6555cdf942c2a
Nic Dart <nicholasjdart@gmail.com>
2011-11-10 16:52:25 +0000

    Compass is working!

---
+++ tests/compasstest.py
@@ -0,0 +1,11 @@
+import time
+from systemetricRobot import SystemetricRobot
+
+def main:
+    R = SystemetricRobot()
+    target = 0
+    
+    while True:
+        heading = robot.heading
+        print heading
+        time.sleep(0.2)
