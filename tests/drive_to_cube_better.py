5b2f70b20c9691a85144db629a71baf5a5551edf
Jonathan Laver <jonathan.laver@gmail.com>
2011-11-10 14:13:52 +0000

    update

--- tests/drive_to_cube_better.py
+++ tests/drive_to_cube_better.py
@@ -11,20 +11,19 @@ def main():
     while True:
         #Get only the tokens
         allMarkers = R.see()
-        markers = [marker for marker in allMarkers if marker.info.marker_type == MARKER_TOKEN] #getting the valid QR code
+        markers = [marker for marker in allMarkers if marker.info.marker_type == MARKER_TOKEN] #filtering the valid QR codes
         time.sleep(0.05)
         if len(markers) != 0:        #if there is A valid QR there...
             R.stop()
             print "Saw the marker"
             angle = markers[0].centre.polar.rot_y    #set the angle of the object (from center)
             if math.fabs(angle) < 10:                #set angle to an absolute value, then drive @ angle
-                R.drive(7)
+                R.drive(sleep)
             else:                                    #else, stop
                 R.turn(-angle)
-                #time.sleep(0.25)
+                time.sleep(0.25)
                 R.stop()
             print "seen at: ",angle
         else:
-            R.turn(7)
-            
+            R.turn(10)
             print "No Marker..."
