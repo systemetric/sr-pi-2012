8ce60a7ac6e8016aefa3ce6fd325c89a9a2f7379
Nic Dart <nicholasjdart@gmail.com>
2011-11-11 16:18:23 +0000

    Made a calibrate thing :)

--- lib/calibratecompass.py
+++ lib/calibratecompass.py
@@ -1,1 +1,17 @@
-#new file
+import time
+import threading
+from lib import compass
+
+from systemetricRobot import SystemetricRobot
+
+def main():
+    R = SystemetricRobot()
+    
+    R.compass.startCalibration()
+    R.turn(10)
+    time.sleep(20)
+    R.compass.stopCalibration()
+    R.stop()
+    while True:
+        print "bearing "+R.compass.heading
+        wait(1)
