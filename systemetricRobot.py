a2fb88d844a44d584a2e7386d5a8bd85430ffbf4
Eric Wieser <wieser.eric@gmail.com>
2011-11-11 15:00:38 +0000

    Abstracted drive away into a new class

--- systemetricRobot.py
+++ systemetricRobot.py
@@ -5,20 +5,17 @@ from collections import namedtuple
 
 from sr import *
 from lib.pyeuclid import *
+from lib.twowheeledrobot import TwoWheeledRobot
 
 Marker = namedtuple("Marker", "vertices normal location")
 Markers = namedtuple("Markers", "tokens robots arena buckets")
 Token = namedtuple("Token", "markers id timestamp location")
 
-class SystemetricRobot(Robot):
+class SystemetricRobot(TwoWheeledRobot):
     '''A class derived from the base 'Robot' class provided by soton'''     
     def __init__(self):
-        #Make sure the soton class is initiated, so we can connect to motors
-        Robot.__init__(self)
-        
-        #Name the motors, for easy access
-        self.leftMotor = self.motors[0]
-        self.rightMotor = self.motors[1]
+        #Get the motors set up
+        TwoWheeledRobot.__init__(self)
         
         #set up the serial connection to the mbed
         try:
@@ -40,42 +37,7 @@ class SystemetricRobot(Robot):
         #Position and orientation of the robot
         self.robotMatrix = Matrix3()
     
-    #Getters and setters for left motor. Allows you to write R.left = 100 for full speed forward
-    @property
-    def left(self):
-        '''Property to control the speed of the left motor, without the hassle of `.target`'''
-        return self.leftMotor.target
-        
-    @left.setter
-    def left(self, value):
-        if not math.isnan(value):
-            self.leftMotor.target = value
-        
-    @property  
-    def right(self):
-        '''Property to control the speed of the right motor, without the hassle of `.target`'''
-        return self.rightMotor.target
-        
-    @right.setter    
-    def right(self, value):
-        if not math.isnan(value):
-            self.rightMotor.target = value
-    
-    def stop(self):
-        '''Stop the robot, by setting the speed of both motors to 0'''
-        self.right = 0
-        self.left = 0
-        
-    def drive(self, speed=50, steer=0):
-        '''Drive the robot forwards or backwards at a certain speed, with an optional steer'''
-        self.right = speed - steer
-        self.left = speed + steer
-        
-    def turn(self, speed):
-        '''Rotate the robot at a certain speed. Positive is clockwise'''
-        self.right = -speed
-        self.left = speed
-    
+   
     @property 
     def compassHeading(self):
         '''Get the compass heading from the mbed'''
