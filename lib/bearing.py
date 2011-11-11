2c85cd9a76cb43f583c8c4312aba03cab84998d1
Eric Wieser <wieser.eric@gmail.com>
2011-11-11 14:44:19 +0000

    Now with operator overloading!

--- lib/bearing.py
+++ lib/bearing.py
@@ -1,3 +1,32 @@
+import math
+
 class Bearing(object):
+    '''Stores an angle between -180 and 180 degrees'''
     def __init__(self, angle):
-        self.angle = angle
+        while angle >= 180:
+            angle -= 360
+        while angle < -180:
+            angle += 360
+        self.degrees = float(angle)
+  
+    @property
+    def radians(self):
+        '''Get the angle in radians'''
+        return math.radians(self.degrees)
+        
+    def __float__(self):
+        '''Make it easy to use as a number'''
+        return self.degrees
+        
+    def __add__(self, other):
+        return Bearing(float(self) + float(other))
+        
+    def __sub__(self, other):
+        return Bearing(float(self) - float(other))
+        
+    def __repr__(self):
+        return u"%.1f\u00B0".encode('utf-8')%(self.degrees)
+    
+
+# print Bearing(20)
+# print Bearing(-20) - Bearing(20)
