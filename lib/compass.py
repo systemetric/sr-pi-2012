48a443aa6b650dbfdee8fc73b6d8aafde4717005
Eric Wieser <wieser.eric@gmail.com>
2011-11-11 15:29:21 +0000

    Moved into new class

---
+++ lib/compass.py
@@ -0,0 +1,27 @@
+import serial
+from lib.bearing import Bearing
+
+class Compass(object):
+    def __init__(port = '/dev/ttyACM0'):
+        try:
+            self.port = serial.Serial(port)
+            self.port.timeout = 0.25
+            self.port.open()
+        except Exception, c:
+            raise Exception('Cannot connect to mbed')
+
+    @property
+    def heading(self):
+        '''Get the compass heading from the mbed'''
+        self.port.write('H')
+        heading = self.port.readline()
+        if heading:
+            return Bearing(int(heading) / 10.0) #convert the int we get from the mbed into a float.
+        else:
+            return Bearing(float('nan')) #return NaN, because we don't know the heading
+    
+    def startCalibration(self):
+        self.port.write('C')
+        
+    def endCalibration(self):
+        self.port.write('C')
