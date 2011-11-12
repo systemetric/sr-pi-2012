cef6dc211c53a1f9bdf19188af9b8764f741eb7c
Jonathan Laver <jonathan.laver@gmail.com>
2011-11-12 15:16:33 +0000

    create

---
+++ tests/speed.py
@@ -0,0 +1,19 @@
+from sr import *
+import time
+from systemetricRobot import SystemetricRobot
+
+def main()
+    R = SystemetricRobot()
+    R.drive(100)
+    time.sleep(2)
+    
+    R.drive(75)
+    time.sleep(2)
+    
+    R.drive(50)
+    time.sleep(2)
+    
+    R.drive(25)
+    time.sleep(2)
+    
+    R.stop()
