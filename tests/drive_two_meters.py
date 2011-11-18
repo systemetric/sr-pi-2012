6c5c25bde443557e89e2a3ef07df1602af5397c3
Nic Dart <nicholasjdart@gmail.com>
2011-11-18 15:29:35 +0000

    ipdate

--- tests/drive_two_meters.py
+++ tests/drive_two_meters.py
@@ -3,11 +3,11 @@ import time
 import systemetric
 
 def main():
-	R = systemetric.Robot()
+    R = systemetric.Robot()
 
-	R.driveDistance(2)
-	R.stop()
-	time.sleep(2)
-	R.rotateBy(180)
-	R.driveDistance(2)
-	R.stop()
+    R.driveDistance(2)
+    R.stop()
+    time.sleep(2)
+    R.rotateBy(180)
+    R.driveDistance(2)
+    R.stop()
