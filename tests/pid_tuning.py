c2eef42e7bcde8c105b5c683a2d6689c59f4481d
Systemetric <robot-society@hillsroad.ac.uk>
2011-11-24 17:20:25 +0000

    Think I found the critical P value

--- tests/pid_tuning.py
+++ tests/pid_tuning.py
@@ -10,11 +10,17 @@ def main():
 	regulator.kd = 0
 	regulator.target = 0
 
-	for p in range(18, 25, 1):
-		regulator.kp = p / 10.0
-		print p / 10.0
-		regulator.enabled = True
-		time.sleep(5)
-		regulator.enabled = False
-		R.stop()
-		time.sleep(1)
+	regulator.kp = 2.25
+	regulator.enabled = True
+	time.sleep(20)
+	regulator.enabled = False
+	R.stop()
+
+	#for p in range(18, 25, 1):
+	#	regulator.kp = p / 10.0
+	#	print p / 10.0
+	#	regulator.enabled = True
+	#	time.sleep(5)
+	#	regulator.enabled = False
+	#	R.stop()
+	#	time.sleep(1)
