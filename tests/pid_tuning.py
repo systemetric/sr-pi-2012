7eef3d1197d620077751188c55df0add2f0b7509
Jonathan Laver <jonathan.laver@gmail.com>
2011-11-24 16:32:23 +0000

    renamed

---
+++ tests/pid_tuning.py
@@ -0,0 +1,13 @@
+from systemetric.compassrobot import *
+import time
+
+def main():
+    regulator = CompassRobot().regulator
+
+    regulator.kp = 1
+    regulator.ki = 0
+    regulator.kd = 0
+    regulator.target = 0
+
+    while True:
+        pass
