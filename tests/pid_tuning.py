979e26e87d4e3ba139855ef0207d7f14fca05493
Systemetric <robot-society@hillsroad.ac.uk>
2011-11-24 17:15:03 +0000

    Fixed indentation

--- tests/pid_tuning.py
+++ tests/pid_tuning.py
@@ -4,17 +4,17 @@ import time
 def main():
 	R = CompassRobot()
 
-    regulator = R.regulator
+	regulator = R.regulator
 
-    regulator.ki = 0
-    regulator.kd = 0
-    regulator.target = 0
+	regulator.ki = 0
+	regulator.kd = 0
+	regulator.target = 0
 
-    for p in range(18, 25, 1):
-    	regulator.kp = p / 10.0
-    	print p / 10.0
-    	regulator.enabled = True
-    	time.sleep(5)
-    	regulator.enabled = False
-    	R.stop()
-    	time.sleep(1)
+	for p in range(18, 25, 1):
+		regulator.kp = p / 10.0
+		print p / 10.0
+		regulator.enabled = True
+		time.sleep(5)
+		regulator.enabled = False
+		R.stop()
+		time.sleep(1)
