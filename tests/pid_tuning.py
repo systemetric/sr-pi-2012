f74167ea5115109d9706e18289a18a384c95b780
Jonathan Laver <jonathan.laver@gmail.com>
2011-11-24 17:08:20 +0000

    fixed range

--- tests/pid_tuning.py
+++ tests/pid_tuning.py
@@ -8,9 +8,9 @@ def main():
     regulator.kd = 0
     regulator.target = 0
 
-    for p in range(1.8, 2.2, 0.1):
-    	regulator.kp = p
-    	print p
+    for p in range(18, 22, 1):
+    	regulator.kp = p / 10.0
+    	print p / 10.0
     	regulator.enabled = True
     	time.sleep(5)
     	regulator.enabled = False
