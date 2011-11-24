5abaabc154c518567b5affb7359ae81d4ca350f0
Jonathan Laver <jonathan.laver@gmail.com>
2011-11-24 17:12:16 +0000

    changed

--- tests/pid_tuning.py
+++ tests/pid_tuning.py
@@ -2,16 +2,19 @@ from systemetric.compassrobot import *
 import time
 
 def main():
-    regulator = CompassRobot().regulator
+	R = CompassRobot()
+
+    regulator = R.regulator
 
     regulator.ki = 0
     regulator.kd = 0
     regulator.target = 0
 
-    for p in range(18, 22, 1):
+    for p in range(18, 25, 1):
     	regulator.kp = p / 10.0
     	print p / 10.0
     	regulator.enabled = True
     	time.sleep(5)
     	regulator.enabled = False
+    	R.stop()
     	time.sleep(1)
