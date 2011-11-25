d1bf24a816157ce13ea7f9ceee5fb7304ea8a293
Jonathan Laver <jonathan.laver@gmail.com>
2011-11-25 13:04:10 +0000

    removed cursestest

--- tests/cursesTest.py
+++
@@ -1,9 +0,0 @@
-import curses
-from systemetric import Robot
-def main():
-	stdscr = curses.initscr()
-	curses.echo()
-
-	while 1:
-	    c = stdscr.getch()
-	    if c == ord('q'): break; curses.nocbreak(); stdscr.keypad(0); curses.echo(); curses.endwin()
