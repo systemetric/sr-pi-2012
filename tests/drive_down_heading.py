c466065a704c09b3cabbdfed16175eb375d01935
Louis Taylor <kragniz@gmail.com>
2011-12-03 11:55:09 +0000

    made the 'drive down heading' code work better

--- tests/drive_down_heading.py
+++ tests/drive_down_heading.py
@@ -5,7 +5,6 @@ import time
 def main():
 	R = systemetric.Robot()
 	speed = 25
-	while True:
-		R.rotateTo(0)
-		R.drive(speed)
-		time.sleep(1)
+	R.rotateTo(R.compass.heading)
+	R.setSpeed(speed)
+	time.sleep(1)
