a9f81498eab9463b9e953d657897052bef0d2e82
Eric Wieser <wieser.eric@gmail.com>
2011-12-05 12:47:35 +0000

    Trying to load the module to run from a config file

--- robot.py
+++ robot.py
@@ -1,4 +1,10 @@
-#import libs.calibratecompass as calibrate
+import json
 
-#calibrate.main()
-import tests
+#Load the config file
+config = json.load('config.json')
+
+#Import the module
+__import__(config.execute)
+
+#Run its main method
+sys.modules[config.execute].main()
