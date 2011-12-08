9e2406864cfa00a6e20bf7d49f3eea7ffdf56502
Jonathan Laver <jonathan.laver@gmail.com>
2011-12-08 17:12:46 +0000

    Got robot positioning working for the single marker case

--- tests/arena.py
+++ tests/arena.py
@@ -4,29 +4,39 @@ from libs.pyeuclid import *
 import math
 
 MARKERS_POSITIONS = {
-	0: Point2(0, 0),
-	1: Point2(1, 0),
-	4: Point2(2, 0),
-	9: Point2(3, 0)
+	9: Point2(0, 0),
+	4: Point2(1, 0),
+	0: Point2(2, 0)
 }
 robotPosition = Point2(0, 0)
+def positionFromMarker(marker):
+	markerId = marker.info.offset
+	markerPosition = MARKERS_POSITIONS.get(markerId)
+	if markerPosition:
+		angleSeenAt = marker.rot_y
+		relativeRotation = marker.orientation.rot_y
+
+		#In radians
+		angle = math.radians(angleSeenAt + (90 - relativeRotation))
+
+		#print "Angles: %.1f %.1f" % (angleSeenAt, relativeRotation)
+		distance = marker.dist
+		robotPosition = Point2(math.cos(angle) * distance, math.sin(angle) * distance)
+
+		print 'pos:', robotPosition + markerPosition, 'angle:', angle
 
 def main():
 	R = systemetric.Robot()
-	angle = 0
-	distance = 0
 
 	while True:
 		markers = R.see(res = (1280, 1024))
-		
-		if markers:
-			angle = markers[0].rot_y + markers[0].orientation.rot_y
-			print markers[0].rot_y, markers[0].orientation.rot_y
-			distance = markers[0].dist
-			robotPosition = Point2(math.sin(angle) * distance, math.cos(angle) * distance)
-
-			print 'pos:', robotPosition, 'angle:', angle
+		print map(markers, positionFromMarker)
+		"""
+		if len(markers) == 1:
+			positionFromMarker(markers[0])
+		elif len(markers) > 1:
+			print len(markers), 'markers'
 		else:
 			print 'no markers'
-
+"""
 		#print [m.orientation.rot_y for m in markers]
