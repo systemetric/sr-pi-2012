e7c28dc9ba61e772bf70c5787ab9c6afff4a0c4b
Eric Wieser <wieser.eric@gmail.com>
2011-12-11 19:32:20 +0000

    Couple of silly errors centered around the use of `self` (or lack of it)

--- systemetric/mapping/arenamap.py
+++ systemetric/mapping/arenamap.py
@@ -1,39 +1,42 @@
 from libs.pyeuclid import *
-
-class ArenaMarker(object):
-	def __init__(center, zone):
-		self.center = center
-		self.zone = zone
-
-		if zone == 0:
-			side = Vector2(marker_width/2, 0)
-		elif zone == 1:
-			side = Vector2(0, marker_width/2)
-		elif zone == 2:
-			side = Vector2(-marker_width/2, 0)
-		elif zone == 3:
-			side = Vector2(0, -marker_width/2)
-
-		self.left, self.right = center + side, center - side
+from pointset import PointSet
 
 class ArenaMap(dict):
+	class Marker(object):
+		def __init__(center, zone):
+			self.center = center
+			self.zone = zone
+
+			if zone == 0:
+				side = Vector2(marker_width/2, 0) #right
+			elif zone == 1:
+				side = Vector2(0, marker_width/2) #up
+			elif zone == 2:
+				side = Vector2(-marker_width/2, 0) #left
+			elif zone == 3:
+				side = Vector2(0, -marker_width/2) #down
+
+			self.left, self.right = center + side, center - side
+
 	def __init__(self, arenaMarkers):
 		self.update(arenaMarkers)
 
-	def nearestMarkerTo(point):
+	def nearestMarkerTo(self, point):
 		"""Find the marker with the minimum distance to the given point, and return its id"""
 		return min(self, key = lambda i: abs(point - self[i].center))
 
-	def positionsFromCodes(codes):
+	def positionsFromCodes(self, codes):
 		positions = []
 		for code in codes:
 			pos = self[code]
 			positions += [pos.right, pos.left]
-		return positions
+		return PointSet(positions)
 
 	def estimatePositionFrom(visionResult):
-		"Calculate the robot position given the"
+		"""Calculate the robot position given what it can see"""
+		#commonMarkerIds = set(visionResult.keys()) & set(self.keys())
 		apparent = visionResult.arenaMarkerEdges()
-		actual = positionsFromCodes(visionResult)
+		actual = self.positionsFromCodes(visionResult)
 
-		return actual.bestTransformTo(apparent) * Point2()
+		#Translation is the amount the origin is transformed
+		return actual.bestTransformTo(apparent) * Point2(0, 0)
