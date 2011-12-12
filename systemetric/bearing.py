a4dbe858349b551b5dd94c2eda83b41c18df58c9
Eric Wieser <wieser.eric@gmail.com>
2011-12-12 20:19:14 +0000

    Added radians constructor for Bearing, and added a Bearing.toPoint(p) method

--- systemetric/bearing.py
+++ systemetric/bearing.py
@@ -2,18 +2,32 @@ import math
 
 class Bearing(object):
     '''Stores an angle between -180 and 180 degrees'''
-    def __init__(self, angle):
-        while angle >= 180:
-            angle -= 360
-        while angle < -180:
-            angle += 360
-        self.degrees = float(angle)
+    def __init__(self, degrees = None, radians = None):
+        if radians is not None and degrees is not None:
+            raise ValueError("degrees and radians cannot both be set")
+
+        if degrees is None:
+            degrees = math.degrees(radians or 0)
+            
+        while degrees >= 180:
+            degrees -= 360
+        while degrees < -180:
+            degrees += 360
+        self.degrees = float(degrees)
   
     @property
     def radians(self):
         '''Get the angle in radians'''
         return math.radians(self.degrees)
+    
+    @classmethod
+    def toPoint(cls, p):
+        '''Get the bearing from the origin to a given point'''
+        angle = math.atan2(p.x, p.y)
+        return cls(radians=angle)
         
+    ofVector = toPoint
+    
     def __float__(self):
         '''Make it easy to use as a number'''
         return self.degrees
