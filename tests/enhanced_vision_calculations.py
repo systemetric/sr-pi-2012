e2e948067683bbb3e5bbc6579ce333bf09c59b97
Eric Wieser <wieser.eric@gmail.com>
2011-12-13 17:00:54 +0000

    Fixed the first set of errors

--- tests/enhanced_vision_calculations.py
+++ tests/enhanced_vision_calculations.py
@@ -2,9 +2,10 @@ import systemetric
 
 def main():
 	R = systemetric.Robot()
+	assert 1 == 3
 	while True:
-		markers = R.see()
-		print markers.arena
+		markers = R.see().processed()
+		print markers.tokens
 		#print len(markers), markers
 		#print markers.arenaMarkerEdges()
 		#print "==========="
