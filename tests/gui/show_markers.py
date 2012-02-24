24cf5d809f84911d6b406863ab118fedc1fc7f9f
Eric Wieser <wieser.eric@gmail.com>
2012-02-24 08:30:25 +0000

    small changes

--- tests/gui/show_markers.py
+++ tests/gui/show_markers.py
@@ -1,6 +1,7 @@
-import systemetric
-import gtk, gobject, cairo
+#import systemetric
+import gtk, gobject, cairo, math
 from systemetric.mapping.arenamaps import S007ArenaMap
+from libs.pyeuclid import *
 
 class Screen(gtk.DrawingArea):
     """ This class is a Drawing Area"""
@@ -26,35 +27,67 @@ class CubeDisplay(Screen):
     def __init__(self):
         super(CubeDisplay, self).__init__()
         self.allTokens = {}
-        self.R = systemetric.Robot()
+       # self.R = systemetric.Robot()
         self.arenaMap = S007ArenaMap()
+        self.tokens = {2: Point2(1, 1), 12: Point2(0.5, 1.75), 0: Point2(3, 1.25)}
 
     def draw(self, w, h):
-    	self.width = w
-    	self.height = h
-    	self.draw_arena(self.cr)
+        self.cr.save()
+        self.initScaling(self.cr, w, h)
+    	self.drawArena(self.cr)
+        self.drawTokens(self.cr)
+        self.cr.restore()
 
-    def draw_robot(self, cr):
+    def drawRobot(self, cr):
         cr.save()
         cr.rectangle(-0.3, -0.3, 0.6, 0.6)
         cr.set_source_rgb(1, 0, 0) 
         cr.fill()
 
-    def draw_arena(self, cr):
-    	cr.save()
-    	cr.set_line_width(0.05)
+    def initScaling(self, cr, w, h):
         matrix = cairo.Matrix()
-        matrix.translate(self.width / 2, self.height / 2)
-        matrix.scale(20, 20)
-        matrix.translate(-self.size.x / 2, -self.size.y / 2)
+
+        #Move to the middle of the screen
+        matrix.translate(w/2, h/2)
+
+        actualWidth, actualHeight = self.arenaMap.size.xy
+        scaleFactor = min(w/actualWidth, h/actualHeight)
+
+        #Scale so that a measurement of 1 corresponds to one meter. Flip Y axis to give normal coords
+        matrix.scale(scaleFactor, -scaleFactor)
+
+        #Move the middle of the arena to the middle of the screen
+        matrix.translate(-actualWidth / 2, -actualHeight / 2)
         cr.transform(matrix)
 
+    def drawArena(self, cr):
+    	cr.save()
+    	cr.set_line_width(0.01)
     	cr.set_source_rgb(1, 0, 0) 
         for key, marker in self.arenaMap.iteritems():
             cr.move_to(marker.left.x, marker.left.y)
             cr.line_to(marker.right.x, marker.right.y)
 
         cr.stroke()
+        cr.restore()
+
+    def drawTokens(self, cr):
+        cr.save()
+        cr.set_source_rgb(0, 0, 1)
+
+        for id, token in self.tokens.iteritems():
+            cr.arc(token.x, token.y, 0.05, 0.0, 2 * math.pi)
+            cr.fill()
+
+        cr.set_source_rgb(1, 1, 1)
+        cr.set_font_size(0.1)
+        m = cairo.Matrix()
+        m.scale(1, -1)
+        cr.transform(m)
+        for id, token in self.tokens.iteritems():
+            cr.move_to(token.x, token.y)
+            cr.show_text(str(id))
+
         cr.restore()
 
     def update_pos(self, event=None):
