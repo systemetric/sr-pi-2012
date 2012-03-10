12d9c10e08a50b3ed116352eef54712a5f710bf9
Jonathan Laver <jonathan.laver@gmail.com>
2012-03-10 14:45:06 +0000

    fixed import, and added R.stop()

--- tests/bucket.py
+++ tests/bucket.py
@@ -1,13 +1,15 @@
-from systemetric import robot
+from sr import *
+import systemetric
 import time
 
-R = Robot()
+R = systemetric.Robot()
 
 while True:
 	markers = R.see()
-	for m in markers:
-		if m.info.marker_type == MARKER_BUCKET_SIDE:
-			 R.driveTo(m.center, gap=0.2)
+	for m in markers.bucketSides:
+		if m == MARKER_BUCKET_SIDE:
+			 R.driveTo(m.centre, gap=0.2)
 			 R.driveDistance(1)
 	time.sleep(1)
 	R.rotateBy(20)
+	R.stop()
