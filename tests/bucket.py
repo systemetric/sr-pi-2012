6a2de2cbb7b962735af72d73c3fd939b193ba6f0
Jonathan Laver <jonathan.laver@gmail.com>
2012-03-12 16:36:39 +0000

    drive towards ship

--- tests/bucket.py
+++ tests/bucket.py
@@ -1,15 +1,17 @@
 import systemetric
 import time
 from systemetric.vision import ProcessedVisionResult
+from pyeuclid import *
 
 R = systemetric.Robot()
+
+def planarLocationOf(self, point):
+	return Point2(point.x, point.z)
 
 while True:
 	markers = R.see()
 	for m in markers.bucketSides:
-		target = ProcessedVisionResult.planarLocationOf(
-			None, m.center + m.normal * 0.5
-		)
+		target = planarLocationOf(m.center + m.normal * 0.5)
 		R.driveTo(target)
 	time.sleep(1)
 	R.rotateBy(20)
