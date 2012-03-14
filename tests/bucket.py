31191c118e953d616bec7dc3219753f4b1f7f1a5
Eric Wieser <wieser.eric@gmail.com>
2012-03-14 15:53:36 +0000

    Some changes to the bucket logic
    Looking as though we want to check that bucket.center and bucket.desirablerobotTargets are correct

--- tests/bucket.py
+++ tests/bucket.py
@@ -1,28 +1,59 @@
 import systemetric
 import time
+from systemetric import Bearing
 from systemetric.vision import ProcessedVisionResult
 from libs.pyeuclid import *
 
 R = systemetric.Robot()
 
-while True:
-	markers = R.see()
-	for b in markers.processed().buckets:
-		target = min(b.desirableRobotTargets, key=abs)
-		targetFacing = b.center - target
-		R.driveTo(target)
-		time.sleep(0.2)
-		
-		angleDifference = Bearing.ofVector(targetFacing) - Bearing.ofVector(target)
-		R.rotateBy(angleDifference)
-		R.stop()
-		time.sleep(0.2)
-		
-		R.drive(15)
+STEPDIST = 1
+try:
+	while True:
+		markers = R.see().processed()
+		buckets = markers.buckets
+
+		if buckets:
+			b = buckets[0]
+
+			target = min(b.desirableRobotTargets, key=abs)
+
+			if abs(target) < 0.1:
+				#Nearly at the target, don't move any more
+				angle = Bearing.ofVector(b.center)
+
+				if abs(angle) > 5:
+					#Need to turn a bit more to face the bucket
+					R.rotateBy(angle)
+
+				#Angle is ok now - go for it!
+				R.drive(15)
+				time.sleep(1)
+				R.stop()
+				break;
+
+			else:
+				if abs(target) > STEPDIST:
+					#Too far from target - try and get closer
+					R.turnToFace(target)
+					R.driveDistance(STEPDIST)
+
+				else:
+					#One more movement
+					R.driveTo(target)
+					time.sleep(0.1)
+
+				#Turn to face where we think the bucket should be, so we can see it next loop
+				targetFacing = b.center - target
+				angleDifference = Bearing.ofVector(targetFacing) - Bearing.ofVector(target)
+				R.rotateBy(angleDifference)
+				R.stop()
+			
 		time.sleep(1)
+		R.rotateBy(20)
+
 		R.stop()
 
-	time.sleep(1)
-	R.rotateBy(20)
+	R.power.beep(440, 1)
 
+except:
 	R.stop()
