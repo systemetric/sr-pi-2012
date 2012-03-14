5181f0f504fbcb5d88cbb1d8d050182b05b81ec7
Eric Wieser <wieser.eric@gmail.com>
2012-03-14 16:21:47 +0000

    Made the bucket code more correct.
    Instead of turning to face where the bucket would be had the robot driven all the way to the target, it now turns to face where the bucket is, given that it sometimes only drives part of the way towards it.
    
    Also, taping a phone to a robot is not a good way to get a good video

--- tests/bucket.py
+++ tests/bucket.py
@@ -7,53 +7,55 @@ from libs.pyeuclid import *
 R = systemetric.Robot()
 
 STEPDIST = 1
-try:
-	while True:
-		markers = R.see().processed()
-		buckets = markers.buckets
+while True:
+	markers = R.see().processed()
+	buckets = markers.buckets
 
-		if buckets:
-			b = buckets[0]
+	if buckets:
+		b = buckets[0]
 
-			target = min(b.desirableRobotTargets, key=abs)
+		target = min(b.desirableRobotTargets, key=abs)
 
-			if abs(target) < 0.1:
-				#Nearly at the target, don't move any more
-				angle = Bearing.ofVector(b.center)
+		print "Bucket seen at %s" % b.center
+		print "Driving to %s" % target
 
-				if abs(angle) > 5:
-					#Need to turn a bit more to face the bucket
-					R.rotateBy(angle)
+		if abs(target) < 0.1:
+			#Nearly at the target, don't move any more
+			print "Nearly there"
+			angle = Bearing.ofVector(b.center)
 
-				#Angle is ok now - go for it!
-				R.drive(15)
-				time.sleep(1)
-				R.stop()
-				break;
 
-			else:
-				if abs(target) > STEPDIST:
-					#Too far from target - try and get closer
-					R.turnToFace(target)
-					R.driveDistance(STEPDIST)
+			if abs(angle) > 5:
+				#Need to turn a bit more to face the bucket
+				print "Off by %s" % angle
+				R.rotateBy(angle)
 
-				else:
-					#One more movement
-					R.driveTo(target)
-					time.sleep(0.1)
+			#Angle is ok now - go for it!
+			print "Success!"
+			R.drive(15)
+			time.sleep(1)
+			R.stop()
+			break;
 
-				#Turn to face where we think the bucket should be, so we can see it next loop
-				targetFacing = b.center - target
-				angleDifference = Bearing.ofVector(targetFacing) - Bearing.ofVector(target)
-				R.rotateBy(angleDifference)
-				R.stop()
-			
-		time.sleep(1)
-		R.rotateBy(20)
+		else:
+			if abs(target) > STEPDIST:
+				#Too far from target - limit movement
+				target = STEPDIST * target.normalize()
 
-		R.stop()
+			#One more movement
+			R.driveTo(target)
+			time.sleep(0.1)
 
-	R.power.beep(440, 1)
+			#Turn to face where we think the bucket should be, so we can see it next loop
+			targetFacing = b.center - target
+			angleDifference = Bearing.ofVector(targetFacing) - Bearing.ofVector(target)
+			print "Turning to face bucket again"
+			R.rotateBy(angleDifference)
+			R.stop()
+		
+	time.sleep(1)
+	R.rotateBy(20)
 
-except:
 	R.stop()
+
+R.power.beep(440, 1)
