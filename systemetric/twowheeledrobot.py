9fc78cd3a3e9fd2ee13941eb35239b0a311cd9b7
Jonathan Laver <jonathan.laver@gmail.com>
2012-04-02 14:31:00 +0100

    left and right were flipped

--- systemetric/twowheeledrobot.py
+++ systemetric/twowheeledrobot.py
@@ -40,11 +40,10 @@ class TwoWheeledRobot(Robot):
         
     def drive(self, speed=50, steer=0):
         '''Drive the robot forwards or backwards at a certain speed, with an optional steer'''
-        self.right = speed - steer
-        self.left = speed + steer
+        self.right = speed + steer
+        self.left = speed - steer
         
     def turn(self, speed):
         '''Rotate the robot at a certain speed. Positive is clockwise'''
-        self.right = -speed
-        self.left = speed
-    
+        self.right = speed
+        self.left = -speed
