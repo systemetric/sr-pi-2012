d59e8108abed1e62a8c158e13a6bc56d559302c0
Louis Taylor <kragniz@gmail.com>
2012-04-15 12:33:46 +0100

    Added licence headers, readme and full licence.

--- systemetric/logs.py
+++ systemetric/logs.py
@@ -1,21 +1,23 @@
-import os, time, sys, functools, inspect, time, collections, threading, contextlib, itertools
+# This file is part of systemetric-student-robotics.
+
+# systemetric-student-robotics is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# systemetric-student-robotics is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with systemetric-student-robotics.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import os, time, sys, functools, inspect, contextlib, itertools
 
 _robotStarted = time.time()
 _roundStarted = None
-
-# class ThreadedOutputStream(object):
-# 	def __init__(self, inner):
-# 		self.streams = collections.defaultdict(lambda: inner)
-
-# 	@property
-# 	def stream(self): return self.streams[threading.current_thread()]
-# 	@stream.setter
-# 	def stream(self, s): self.streams[threading.current_thread()] = s
-# 	@stream.deleter
-# 	def stream(self): del self.streams[threading.current_thread()]
-
-# 	def write(self, data):
-# 		self.stream.write(data)
 
 class StreamWrapper(object):
 	"""
