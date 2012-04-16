3462b6bbb72df5add3bd51fff7a18ef2c4f8ad02
Eric Wieser <wieser.eric@gmail.com>
2012-04-16 13:11:39 +0100

    Added documentation to the mbed stuff

--- systemetric/devices/mbed.py
+++ systemetric/devices/mbed.py
@@ -17,38 +17,62 @@ import serial
 from threading import Lock
 
 class MbedDevice(object):
-	def __init__(self, id, mbed):
-	    self.mbed = mbed or Mbed.get()
-	    self.id = id
+	"""
+	A device attached to an mbed. The protocol is to send a single character
+	representing the device ID, then another (if necessary) to specify what
+	information should be retrieved. Arguments can be given in a function-
+	like syntax. For example:
+
+		MbedDevice('G').request('r', 10) -> Gr(10)
+		MbedDevice('C').request('h')      -> Ch
+
+	The mbed then returns a single line response. The line may be empty
+	"""
+	def __init__(self, id, mbed=None):
+		"""Construct a device with a given id"""
+		self.mbed = mbed or Mbed.get()
+		self.id = id
 
 	def request(self, thing='', *args):
-	    command = self.id + thing
-	    if args:
-	        command += '(' + ','.join(str(r) for r in args) + ')'
-	    
-	    return self.mbed.sendCommand(command)
+		"""Request something from the mbed"""
+		command = self.id + thing
+		if args:
+			command += '(' + ','.join(str(r) for r in args) + ')'
+		
+		return self.mbed.sendCommand(command)
 
 class Mbed(object):
-	_mainMbed = None
+	"""The mbed itself, and basic communications with it"""
 	def __init__(self, port):
-	    try:
-	        self.port = serial.Serial(port)
-	       # self.port.timeout = 0.25
-	        self.port.open()
-	        self._lock = Lock()
-	    except Exception:
-	        raise Exception('Cannot connect to mbed on %s' % port)
+		try:
+			self.port = serial.Serial(port)
+			# self.port.timeout = 0.25
+			self.port.open()
+			self._lock = Lock()
+		except Exception:
+			raise Exception('Cannot connect to mbed on %s' % port)
 
 
 	def sendCommand(self, c):
-	    with self._lock:
-	        self.port.flushInput()
-	        self.port.write(c)
-	        return self.port.readline()
+		"""
+		Send a short command, and return a single line response. Prevents other
+		threads interweaving requests by locking on self._lock
+		"""
+		with self._lock:
+			self.port.flushInput()
+			self.port.write(c)
+			return self.port.readline()
+
+	_mainMbed = None
 
 	@classmethod
 	def get(cls):
-	    if not cls._mainMbed:
-	        _mainMbed = cls('/dev/ttyACM0')
+		"""
+		A lazy singleton, using the default port. This should be used instead
+		of calling the constructor, to prevent the serial port being opened
+		twice
+		"""
+		if not cls._mainMbed:
+			_mainMbed = cls('/dev/ttyACM0')
 
-	    return _mainMbed     
+		return _mainMbed     
